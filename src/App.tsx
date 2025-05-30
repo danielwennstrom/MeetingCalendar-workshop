1cf146d49d86ed2314bb0e8d06164e727f7e7645
Daniel Wennström <daniel@wennstrom.io>
2025-05-30 10:47:00 +0200

    Removed cancel button, changed text on save button

--- src/App.tsx
+++ src/App.tsx
@@ -221,16 +221,10 @@ function App() {
                 <div className="col-span-full">
                   <div className="mt-6 flex items-center justify-end gap-x-6 ">
                     <button
-                      type="button"
-                      className="text-sm/6 font-semibold text-gray-900"
-                    >
-                      Cancel
-                    </button>
-                    <button
                       type="submit"
                       className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      Save
+                      Create Meeting
                     </button>
                   </div>
                 </div>
