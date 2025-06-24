1b925cbe0b7804a7d4bea0a04e1c317b4ddf56b0
danielwennstrom <daniel@wennstrom.io>
2025-06-24 16:34:41 +0200

    Made sign-up form reusable for editing user details, edit user feature

---
+++ src/components/Users/CreateUserForm.tsx
@@ -0,0 +1,208 @@
+import React from "react";
+import type { UserAuthRegister } from "../../types/UserAuthRegister";
+import { useForm } from "react-hook-form";
+import type { User } from "../../types/User";
+import api from "../../services/api";
+
+type Props = {
+  onEdit?: (editing: User) => void;
+  editingData?: User | undefined;
+  onRegister?: (newUser: UserAuthRegister) => void;
+  onClose?: () => void;
+  isEditMode?: boolean;
+};
+
+function CreateUserForm({ editingData, onClose, isEditMode, onEdit }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserAuthRegister>({ defaultValues: editingData });
+
+  const onSubmit = async (data: UserAuthRegister) => {
+    try {
+      if (isEditMode && onEdit !== undefined) {
+        const updatedUser: User = {
+          ...editingData!,
+          ...data,
+        };
+
+        onEdit(updatedUser);
+      } else {
+        const response = await api.post("/auth/register", data);
+        const token = response.data;
+        localStorage.setItem("access_token", token);
+        window.location.href = "/";
+      }
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  };
+
+  const passwordPattern =
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="mt-7 flex flex-col justify-center space-y-6 sm:min-w-sm mx-auto"
+    >
+      <div>
+        <label
+          htmlFor="userName"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          User name
+        </label>
+        <div className="mt-2">
+          <input
+            id="userName"
+            type="text"
+            {...register("username", {
+              required: "User name is required",
+              pattern: {
+                value: /^[a-zA-Z0-9_]{3,20}$/,
+                message:
+                  "User name must be 3-20 characters, letters, numbers, or underscores only",
+              },
+            })}
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+          {errors.username && (
+            <span className="text-red-500">{errors.username.message}</span>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          Full name
+        </label>
+        <div className="mt-2">
+          <input
+            id="fullname"
+            type="text"
+            {...register("profile.name", {
+              required: true,
+              pattern: {
+                value: /^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,50}$/,
+                message:
+                  "Full name must be 2–50 letters long and may include spaces, hyphens, and apostrophes only",
+              },
+            })}
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+          {errors.profile?.name && (
+            <span className="text-red-500">{errors.profile.name.message}</span>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <label
+          htmlFor="email"
+          className="block text-sm/6 font-medium text-gray-900"
+        >
+          Email address
+        </label>
+        <div className="mt-2">
+          <input
+            id="email"
+            type="email"
+            {...register("profile.email", {
+              required: "E-mail address is required",
+            })}
+            autoComplete="email"
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+          {errors.profile?.email && (
+            <span className="text-red-500">{errors.profile.email.message}</span>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="phoneNumber"
+            className="block text-sm/6 font-medium text-gray-900"
+          >
+            Phone number
+          </label>
+        </div>
+        <div className="mt-2">
+          <input
+            id="phoneNumber"
+            type="text"
+            {...register("profile.phoneNumber", {
+              required: "Phone number is required",
+              pattern: {
+                value: /^\+?[0-9\s\-().]{7,20}$/,
+                message: "Enter a valid phone number",
+              },
+            })}
+            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          />
+          {errors.profile?.phoneNumber && (
+            <span className="text-red-500">
+              {errors.profile.phoneNumber.message}
+            </span>
+          )}
+        </div>
+      </div>
+
+      {!isEditMode && (
+        <div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Password
+            </label>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              type="password"
+              {...register("password", {
+                required: "Password is required",
+                pattern: {
+                  value: passwordPattern,
+                  message:
+                    "Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long",
+                },
+              })}
+              autoComplete="current-password"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
+            {errors.password && (
+              <span className="text-red-500">{errors.password.message}</span>
+            )}
+          </div>
+        </div>
+      )}
+      <div>
+        {isEditMode && (
+          <button
+            onClick={onClose}
+            className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+          >
+            Cancel
+          </button>
+        )}
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          {!isEditMode ? <span>Sign up</span> : <span>Update user</span>}
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default CreateUserForm;
---
+++ src/pages/Users.tsx
@@ -0,0 +1,134 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
+import { useEffect, useState } from "react";
+import api from "../services/api";
+import { isAdmin } from "../utils/roleUtils";
+import type { User } from "../types/User";
+import ModalEditUser from "../components/Users/ModalEditUser";
+
+export default function Users() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [editingIdx, setEditingIdx] = useState<number | null>(null);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await api.get(`/user`);
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+      setUsers([]);
+    }
+  }
+
+  function handleEditItem(idx: number) {
+    setEditingIdx(idx);
+  }
+
+  function closeEditModal() {
+    setEditingIdx(null);
+  }
+
+  async function handleSaveItem(updatedUserData: User) {
+    try {
+      await api.put(`/user/${updatedUserData.id}`, updatedUserData);
+
+      closeEditModal();
+      fetchUsers();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <ul role="list" className="flex flex-col w-full divide-y divide-gray-100">
+      {users.map((user, idx: number) => (
+        <li key={user.id} className="flex justify-between gap-x-6 py-5">
+          <div className="flex min-w-0 gap-x-4">
+            {/* <img
+              alt=""
+              src={user.profile.avatarUrl}
+              className="size-12 flex-none rounded-full bg-gray-50"
+            /> */}
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm/6 font-semibold text-gray-900">
+                <a href={user.profile.href} className="hover:underline">
+                  {user.profile.name}
+                </a>
+              </p>
+              <p className="mt-1 flex text-xs/5 text-gray-500">
+                <a
+                  href={`mailto:${user.profile.email}`}
+                  className="truncate hover:underline"
+                >
+                  {user.profile.email}
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="flex shrink-0 items-center gap-x-6">
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm/6 text-gray-900">{user.role}</p>
+              {/* {person.lastSeen ? (
+                <p className="mt-1 text-xs/5 text-gray-500">
+                  Last seen{" "}
+                  <time dateTime={person.lastSeenDateTime}>
+                    {person.lastSeen}
+                  </time>
+                </p>
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div className="size-1.5 rounded-full bg-emerald-500" />
+                  </div>
+                  <p className="text-xs/5 text-gray-500">Online</p>
+                </div>
+              )} */}
+            </div>
+            {isAdmin() && (
+              <Menu as="div" className="relative flex-none">
+                <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                  <span className="sr-only">Open options</span>
+                  <EllipsisVerticalIcon aria-hidden="true" className="size-5" />
+                </MenuButton>
+                <MenuItems
+                  transition
+                  className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                >
+                  <MenuItem>
+                    {/* <a
+                      href="#"
+                      className="block px-3 py-1 text-sm/6 text-gray-900 data-focus:bg-gray-50 data-focus:outline-hidden"
+                    >
+                      View profile
+                      <span className="sr-only">, {user.profile.name}</span>
+                    </a> */}
+                    <button
+                      type="button"
+                      className="flex w-full px-2 py-2 text-s font-semibold text-gray-500 shadow-xs hover:text-gray-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      onClick={() => handleEditItem(idx)}
+                    >
+                      <span>Edit</span>
+                    </button>
+                  </MenuItem>
+                </MenuItems>
+              </Menu>
+            )}
+          </div>
+        </li>
+      ))}
+
+      {editingIdx != null && users[editingIdx] && (
+        <ModalEditUser
+          editingUser={users[editingIdx]}
+          onEdit={handleSaveItem}
+          isEditMode={true}
+          onClose={closeEditModal}
+        />
+      )}
+    </ul>
+  );
+}
